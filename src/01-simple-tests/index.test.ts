543c0d6c2bae7ee9358a3e2afec2f2717d1f0e69
overdd <lmbib3a4@duck.com>
2023-07-01 09:57:57 +0200

    task 4: simple and table tests are implemented

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,40 @@
-// Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
-describe('simpleCalculator tests', () => {
-  test('should add two numbers', () => {
-    // Write your test here
+describe('simpleCalculator', () => {
+  describe('Positive Tests', () => {
+    it('should add two numbers', () => {
+      const input = { a: 1, b: 2, action: Action.Add };
+      expect(simpleCalculator(input)).toEqual(3);
+    });
+    it('should subtract two numbers', () => {
+      const input = { a: 5, b: 3, action: Action.Subtract };
+      expect(simpleCalculator(input)).toEqual(2);
+    });
+    it('should multiply two numbers', () => {
+      const input = { a: 4, b: 2, action: Action.Multiply };
+      expect(simpleCalculator(input)).toEqual(8);
+    });
+    it('should divide two numbers', () => {
+      const input = { a: 9, b: 3, action: Action.Divide };
+      expect(simpleCalculator(input)).toEqual(3);
+    });
+    it('should exponentiate two numbers', () => {
+      const input = { a: 2, b: 3, action: Action.Exponentiate };
+      expect(simpleCalculator(input)).toEqual(8);
+    });
   });
-
-  test('should subtract two numbers', () => {
-    // Write your test here
-  });
-
-  test('should multiply two numbers', () => {
-    // Write your test here
-  });
-
-  test('should divide two numbers', () => {
-    // Write your test here
-  });
-
-  test('should exponentiate two numbers', () => {
-    // Write your test here
-  });
-
-  test('should return null for invalid action', () => {
-    // Write your test here
-  });
-
-  test('should return null for invalid arguments', () => {
-    // Write your test here
+  describe('Negative Tests', () => {
+    it('should return null for invalid action', () => {
+      const input = { a: 2, b: 3, action: 'invalid action' };
+      expect(simpleCalculator(input)).toBeNull();
+    });
+    it('should return null for invalid arguments', () => {
+      const input = { a: 'foo', b: 3, action: Action.Add };
+      expect(simpleCalculator(input)).toBeNull();
+    });
+    it('should return Infinity for divide by zero', () => {
+      const input = { a: 3, b: 0, action: Action.Divide };
+      expect(simpleCalculator(input)).toBeNull();
+    });
   });
 });
--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,39 @@
-// Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+  { a: 1, b: 2, action: Action.Add, expected: 3 },
+  { a: 5, b: 3, action: Action.Subtract, expected: 2 },
+  { a: 4, b: 2, action: Action.Multiply, expected: 8 },
+  { a: 9, b: 3, action: Action.Divide, expected: 3 },
+  { a: 2, b: 3, action: Action.Exponentiate, expected: 8 },
+];
+
+const testCasesNegative = [
+  { a: 2, b: 3, action: 'invalid action', expected: null },
+  { a: 'foo', b: 3, action: Action.Add, expected: null },
+  { a: 2, b: 0, action: Action.Divide, expected: null },
+];
 
 describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
+  describe('Positive Tests', () => {
+    test.each(testCases)(
+      'given a: %p, b: %p and action: %p, returns: %p',
+      ({ a, b, action, expected }) => {
+        expect(simpleCalculator({ a, b, action })).toBe(expected);
+      },
+    );
   });
-  // Consider to use Jest table tests API to test all cases above
+
+  describe('Negative Tests', () => {
+    test.each(testCasesNegative)(
+      'given a: %p, b: %p and action: %p, returns: %p',
+      ({ a, b, action, expected }) => {
+        if (typeof a === 'string' || typeof action === 'string') {
+          expect(simpleCalculator({ a, b, action })).toBe(expected);
+        } else {
+          throw new Error('Invalid test parameters');
+        }
+      },
+    );
+  });
 });
